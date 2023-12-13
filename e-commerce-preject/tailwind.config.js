43d8306d04ac1004bd43f5166e11313c9c29c417
Mgokayay <aymustafagokay@gmail.com>
2023-12-13 17:28:13 +0300

    carousel kütüphanesini import ettim

--- e-commerce-preject/tailwind.config.js
+++ e-commerce-preject/tailwind.config.js
@@ -1,8 +1,18 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{html,js}"],
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+module.exports = withMT({
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "./node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        montserrat: ["Montserrat"],
+      },
+    },
   },
   plugins: [],
-};
+});
