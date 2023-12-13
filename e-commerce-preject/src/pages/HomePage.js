fa62cec50ce1231bba19c9ab155e763095e801dd
Mgokayay <aymustafagokay@gmail.com>
2023-12-13 17:29:37 +0300

    header carosel editorspick import edildi

---
+++ e-commerce-preject/src/pages/HomePage.js
@@ -0,0 +1,15 @@
+import { Carouselll } from "../components/Carouselll";
+import EditorsPick from "../layout/EditorsPick";
+import Header from "../components/Header";
+
+const HomePage = () => {
+  return (
+    <>
+      <Header />
+      <Carouselll />
+      <EditorsPick />
+    </>
+  );
+};
+
+export default HomePage;
