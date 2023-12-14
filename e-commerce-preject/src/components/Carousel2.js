868c63c36f5969129310392d927fc94831724f24
Mgokayay <aymustafagokay@gmail.com>
2023-12-14 20:45:28 +0300

    carousel 2 slider yapıldı

--- e-commerce-preject/src/components/Carousel2.js
+++ e-commerce-preject/src/components/Carousel2.js
@@ -3,17 +3,20 @@ import slider2 from "../assets/sliders/slider2.png";
 
 export function Carousel2() {
   return (
-    <Carousel transition={{ duration: 2 }}>
+    <Carousel
+      className="bg-[#23856D]  flex relative"
+      transition={{ duration: 1 }}
+    >
       <img
         src={slider2}
         alt="slider2"
-        className="h-[calc(100vh_-_134px)] w-full object-cover"
+        className="h-[685px] w-[443px] object-cover absolute left-[60%] "
       />
 
       <img
         src={slider2}
         alt="slider2"
-        className="h-[calc(100vh_-_134px)] w-full object-cover"
+        className="h-[685px] w-[443px] object-cover "
       />
     </Carousel>
   );
