8f273f9989a9b8769e252b588376b7335c7832f2
Mgokayay <aymustafagokay@gmail.com>
2023-12-21 14:50:08 +0300

    team page template oluşturuldu

---
+++ e-commerce-preject/src/pages/TeamPage.js
@@ -0,0 +1,13 @@
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+
+const TeamPage = () => {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+};
+
+export default TeamPage;
