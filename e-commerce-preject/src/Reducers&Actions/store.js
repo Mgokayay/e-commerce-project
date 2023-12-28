952ec84b345d989436a233f60c439b359055cdfc
Mgokayay <aymustafagokay@gmail.com>
2023-12-28 16:36:23 +0300

    reducer ve actionlar store a eklendi

--- e-commerce-preject/src/Reducers&Actions/store.js
+++ e-commerce-preject/src/Reducers&Actions/store.js
@@ -6,9 +6,17 @@ import {
 import logger from "redux-logger";
 import { thunk } from "redux-thunk";
 import { globalReducer } from "./reducers/globalReducer";
+import { userReducer } from "./reducers/userReducer";
+import { storeReducer } from "./reducers/storeReducer";
+import { shoppincCartReducer } from "./reducers/shoppingCartReducer";
+import { productReducer } from "./reducers/productReducer";
 
 export const reducers = combineReducers({
   general: globalReducer,
+  user: userReducer,
+  store: storeReducer,
+  shop: shoppincCartReducer,
+  products: productReducer,
 });
 
 export const store = createStore(reducers, applyMiddleware(thunk, logger));
