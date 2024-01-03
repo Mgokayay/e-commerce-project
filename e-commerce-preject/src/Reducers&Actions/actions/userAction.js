d9dd22f061115e70fa862c0f580dcfee30475dc6
Mgokayay <aymustafagokay@gmail.com>
2024-01-03 15:09:38 +0300

    useraction da logindata oluşturuldu

--- e-commerce-preject/src/Reducers&Actions/actions/userAction.js
+++ e-commerce-preject/src/Reducers&Actions/actions/userAction.js
@@ -1,5 +1,5 @@
 export const SET_USER = "SET_USER";
-
+export const LOGIN_DATA = "LOGIN_DATA";
 export const DELETE_USER = "DELETE_USER";
 
 export const setUser = (data) => ({
@@ -9,5 +9,10 @@ export const setUser = (data) => ({
 
 export const deleteUser = (data) => ({
   type: DELETE_USER,
+  payload: data,
+});
+
+export const loginData = (data) => ({
+  type: LOGIN_DATA,
   payload: data,
 });
