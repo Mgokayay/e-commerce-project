bed45cccd7f2ad21879af4ece8cf27a62bc50728
Mgokayay <aymustafagokay@gmail.com>
2024-01-03 12:21:38 +0300

    global action ve reducer düzeltildi

--- e-commerce-preject/src/Reducers&Actions/actions/globalAction.js
+++ e-commerce-preject/src/Reducers&Actions/actions/globalAction.js
@@ -1,15 +1,5 @@
-export const LOGGED_IN = "LOGGED_IN";
-export const LOGGED_OUT = "LOGGED_OUT";
 export const SET_ROLES = "SET_ROLES";
 export const SET_CATEGORİES = "SET_CATEGORİES";
-
-export const loggedIn = (data) => ({
-  type: LOGGED_IN,
-  payload: data,
-});
-export const loggedOut = () => ({
-  type: LOGGED_OUT,
-});
 
 export const setRoles = (data) => ({
   type: SET_ROLES,
--- e-commerce-preject/src/Reducers&Actions/reducers/globalReducer.js
+++ e-commerce-preject/src/Reducers&Actions/reducers/globalReducer.js
@@ -1,32 +1,14 @@
-import {
-  LOGGED_IN,
-  LOGGED_OUT,
-  SET_ROLES,
-  SET_CATEGORİES,
-} from "../actions/globalAction";
+import { SET_ROLES, SET_CATEGORİES } from "../actions/globalAction";
 
 const globalInitial = {
   roles: [],
   categories: [],
   theme: "",
   language: "",
-  loggedIn: false,
 };
 
 const globalReducer = (state = globalInitial, action) => {
   switch (action.type) {
-    case LOGGED_IN:
-      return {
-        ...state,
-        loggedIn: true,
-      };
-    case LOGGED_OUT:
-      localStorage.removeItem("token");
-      const loggedOut = {
-        ...state,
-        loggedIn: false,
-      };
-      return loggedOut;
     case SET_ROLES:
       return {
         ...state,
