c57970035d443f88a3a704db17c1f3c217ee7340
Mgokayay <aymustafagokay@gmail.com>
2024-01-04 01:59:45 +0300

    shopcards dynamic hale getirildi apiden dönen değerler alınıyor

--- e-commerce-preject/src/components/ShopCards.js
+++ e-commerce-preject/src/components/ShopCards.js
@@ -1,19 +1,24 @@
-import img1 from "../assets/productlistpage/5items1.png";
-import img2 from "../assets/productlistpage/5items2.png";
-import img3 from "../assets/productlistpage/5items3.png";
-import img4 from "../assets/productlistpage/5items4.png";
-import img5 from "../assets/productlistpage/5items5.png";
-
+import { useSelector } from "react-redux";
 const ShopCards = () => {
-  const itemsImg = [img1, img2, img3, img4, img5];
+  const categories = useSelector((state) => state.global.categories);
+  const sortedCategories = categories.sort((a, b) => b.rating - a.rating);
+  const top5Categories = sortedCategories.slice(0, 5);
   return (
     <div className="bg-[#FAFAFA] flex gap-2 justify-center flex-col xl:flex-row pb-8">
-      {itemsImg.map((image, index) => (
-        <img
-          src={image}
-          alt={`card ${index}`}
-          className="cursor-pointer hover:scale-105 duration-200 transition opacity-100 hover:opacity-60"
-        />
+      {top5Categories.map((category, index) => (
+        <a
+          href={`/shopping/${
+            category.code.charAt(0) === "e" ? "erkek" : "kadin"
+          }/${category.title}`}
+          key={index}
+        >
+          <img
+            src={category.img}
+            alt={`card ${index}`}
+            className="cursor-pointer hover:scale-105 duration-200 transition opacity-100 hover:opacity-60"
+          />
+          <h6 className="text-2xl font-bold">{category.title}</h6>
+        </a>
       ))}
     </div>
   );
