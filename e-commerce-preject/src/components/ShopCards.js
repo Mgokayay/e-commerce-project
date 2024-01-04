e131833de2a48414a228ad408bac6165314b9302
Mgokayay <aymustafagokay@gmail.com>
2024-01-05 00:23:08 +0300

    img lar hizalandı

--- e-commerce-preject/src/components/ShopCards.js
+++ e-commerce-preject/src/components/ShopCards.js
@@ -4,7 +4,7 @@ const ShopCards = () => {
   const sortedCategories = categories.sort((a, b) => b.rating - a.rating);
   const top5Categories = sortedCategories.slice(0, 5);
   return (
-    <div className="bg-[#FAFAFA] flex gap-2 justify-center flex-col xl:flex-row pb-8">
+    <div className="bg-[#FAFAFA] flex gap-2 justify-center  flex-col xl:flex-row pb-8">
       {top5Categories.map((category, index) => (
         <a
           href={`/shopping/${
@@ -15,7 +15,7 @@ const ShopCards = () => {
           <img
             src={category.img}
             alt={`card ${index}`}
-            className="cursor-pointer hover:scale-105 duration-200 transition opacity-100 hover:opacity-60"
+            className="cursor-pointer w-48 h-64 hover:scale-105 duration-200 transition opacity-100 hover:opacity-60"
           />
           <h6 className="text-2xl font-bold">{category.title}</h6>
         </a>
