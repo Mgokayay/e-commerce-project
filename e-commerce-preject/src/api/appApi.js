0f8cb7d5768a545fc39702f7cf85b24ac6eb858e
Mgokayay <aymustafagokay@gmail.com>
2024-01-04 16:41:50 +0300

    product fetch için apiden endpoint dönüldü

--- e-commerce-preject/src/api/appApi.js
+++ e-commerce-preject/src/api/appApi.js
@@ -3,6 +3,7 @@ import {
   setCategories,
   setRoles,
 } from "../Reducers&Actions/actions/globalAction";
+import { productFetch } from "../Reducers&Actions/actions/productAction";
 
 export const fetchRoles = () => {
   return (dispatch) => {
@@ -20,6 +21,16 @@ export const fetchCategories = () => {
       .get("https://workintech-fe-ecommerce.onrender.com/categories")
       .then((response) => {
         dispatch(setCategories(response.data));
+      })
+      .catch((error) => {});
+  };
+};
+export const fetchProducts = () => {
+  return (dispatch) => {
+    axios
+      .get("https://workintech-fe-ecommerce.onrender.com/products")
+      .then((response) => {
+        dispatch(productFetch(response.data));
       })
       .catch((error) => {});
   };
