c33919a60522be99a352b93dabaa8c693be2028b
Mgokayay <aymustafagokay@gmail.com>
2024-01-11 19:01:38 +0300

    css te düzeltmeler yapıldı

--- e-commerce-preject/src/components/ProductCard.js
+++ e-commerce-preject/src/components/ProductCard.js
@@ -36,24 +36,28 @@ const ProductCard = () => {
       {products.map((product, index) => (
         <div
           key={index}
-          className="flex flex-col  items-center hover:scale-110 transition duration-300 "
+          className="flex flex-col items-center hover:scale-110 transition duration-200 "
         >
           <img
             className="max-w-96 max-h-96"
             src={product?.images?.[0]?.url}
             alt={`card ${index}`}
           />
-          <h2 className="text-[#252B42] text-base leading-3 font-bold pt-8 pb-4">
-            {product.name}
-          </h2>
-          <p className="text-[#737373] text-sm font-bold pb-4">
-            {product.description}
-          </p>
-          <div className="flex gap-2 pb-4">
-            <p className="text-[#BDBDBD] text-sm font-bold">$16.48</p>
-            <p className="text-[#23856D] text-sm font-bold">{product.price}</p>
+          <div className="max-w-[250px]">
+            <h2 className="text-[#252B42] text-base leading-3 font-bold pt-8 pb-4">
+              {product.name}
+            </h2>
+            <p className="text-[#737373] text-sm font-bold pb-4 ">
+              {product.description}
+            </p>
+            <div className="flex gap-2 pb-4">
+              <p className="text-[#BDBDBD] text-sm font-bold">$16.48</p>
+              <p className="text-[#23856D] text-sm font-bold">
+                {product.price}
+              </p>
+            </div>
+            <img src={svg} alt="svg" className="cursor-pointer" />
           </div>
-          <img src={svg} alt="svg" className="cursor-pointer" />
         </div>
       ))}
     </div>
