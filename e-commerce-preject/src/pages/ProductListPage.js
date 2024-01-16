0b27a6ed01adcabfe44908396090bc1804647b3d
Mgokayay <aymustafagokay@gmail.com>
2024-01-16 23:58:09 +0300

    productlistpage de infinite scroll yapısı tanımlandı

--- e-commerce-preject/src/pages/ProductListPage.js
+++ e-commerce-preject/src/pages/ProductListPage.js
@@ -1,35 +1,52 @@
 import Footer from "../components/Footer";
 import Header from "../components/Header";
-import ProductListMain from "../layout/ProductListMain";
-
+// import ProductListMain from "../layout/ProductListMain";
+import InfiniteScroll from "react-infinite-scroll-component";
 import LinkContainer from "../components/LinkContainer";
 import ShopCards from "../components/ShopCards";
 import FilterContainer from "../components/FilterContainer";
 import Client from "../components/Clients";
 import { useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
-import { fetchProducts } from "../api/appApi";
+import { fetchNextPage, fetchProducts } from "../api/appApi";
+import ProductCard from "../components/ProductCard";
+
 const ProductListPage = () => {
   const dispatch = useDispatch();
   const history = useHistory();
+  const { productList } = useSelector((state) => state.products);
   const [searchTerm, setSearchTerm] = useState("");
+  const [categoryType, setCategoryType] = useState(1);
+  const [sortType, setSortType] = useState("");
+  const [hasMore, setHasMore] = useState(true);
+
   const handleFilterChange = (search) => {
     setSearchTerm(search);
     dispatch(fetchProducts(categoryType, search, sortType));
     setQueryString();
   };
-  const [categoryType, setCategoryType] = useState(1);
+
   const handleCategoryChange = (category) => {
     setCategoryType(category.id);
     dispatch(fetchProducts(category.id, searchTerm, sortType));
     setQueryString(category);
   };
-  const [sortType, setSortType] = useState("");
+
   const handleSortChange = (sort) => {
     setSortType(sort);
     dispatch(fetchProducts(categoryType, searchTerm, sort));
     setQueryString();
+  };
+
+  const fetchData = (data) => {
+    dispatch(fetchNextPage(data))
+      .then(() => {
+        setHasMore(true);
+      })
+      .catch((error) => {
+        setHasMore(false);
+      });
   };
   const setQueryString = (category) => {
     // const genderPrefix = category.code.charAt(0) === "e" ? "erkek" : "kadin";
@@ -53,11 +70,34 @@ const ProductListPage = () => {
         categoryType={categoryType}
         onCategoryChange={handleCategoryChange}
       />
-      <ProductListMain
-        searchTerm={searchTerm}
-        sortType={sortType}
-        categoryType={categoryType}
-      />
+      <InfiniteScroll
+        dataLength={productList.length} //This is important field to render the next data
+        next={() =>
+          fetchData({
+            categoryType,
+            searchTerm,
+            sortType,
+            offset: 25,
+          })
+        }
+        hasMore={hasMore}
+        loader={<h4>Loading...</h4>}
+        endMessage={
+          <p style={{ textAlign: "center" }}>
+            <b>Yay! You have seen it all</b>
+          </p>
+        }
+      >
+        <div className="flex flex-col px-[10%]">
+          <ProductCard />
+        </div>
+      </InfiniteScroll>
+
+      {/* <ProductListMain
+      // searchTerm={searchTerm}
+      // sortType={sortType}
+      // categoryType={categoryType}
+      /> */}
       <Client />
       <Footer />
     </>
