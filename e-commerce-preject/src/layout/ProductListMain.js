d660c3bf2868ea2ce2cb800b3a9de6a0e92b0346
Mgokayay <aymustafagokay@gmail.com>
2024-01-16 23:57:18 +0300

    bir component yapısı sadeleştirildi

--- e-commerce-preject/src/layout/ProductListMain.js
+++ e-commerce-preject/src/layout/ProductListMain.js
@@ -1,16 +1,9 @@
 import ProductCard from "../components/ProductCard";
-import img1 from "../assets/productlistpage/Pagination.png";
-const ProductListMain = ({ searchTerm, sortType, categoryType }) => {
+
+const ProductListMain = () => {
   return (
     <div className="flex flex-col px-[10%]">
-      <ProductCard
-        searchTerm={searchTerm}
-        sortType={sortType}
-        categoryType={categoryType}
-      />
-      <div className="flex justify-center items-center py-20">
-        <img src={img1} alt="img1" />
-      </div>
+      <ProductCard />
     </div>
   );
 };
