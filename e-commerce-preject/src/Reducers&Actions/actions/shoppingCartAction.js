175bcb4b7ebab4c228a90732408853189901f03e
Mgokayay <aymustafagokay@gmail.com>
2024-03-12 19:17:39 +0300

    shoppingCartAction created

--- e-commerce-preject/src/Reducers&Actions/actions/shoppingCartAction.js
+++ e-commerce-preject/src/Reducers&Actions/actions/shoppingCartAction.js
@@ -1,4 +1,19 @@
+export const addToCart = (data) => ({
+  type: "ADD_TO_CART",
+  payload: data,
+});
 
+export const countDecrease = (data) => ({
+  type: "DECREASE_TO_CART",
+  payload: data,
+});
 
+export const removeProduct = (data) => ({
+  type: "REMOVE_TO_CART",
+  payload: data,
+});
 
-
+export const checkedProduct = (data) => ({
+  type: "CHECK_TO_PRODUCT",
+  payload: data,
+});
